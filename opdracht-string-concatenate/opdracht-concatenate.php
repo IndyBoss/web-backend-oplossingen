8ac45bdf6736b8882fb647f06a01df66769b95a1
IndyBoss <32159085+IndyBoss@users.noreply.github.com>
2017-10-05 10:59:25 +0200

    opdracht-concatenate

---
+++ opdracht-string-concatenate/opdracht-concatenate.php
@@ -0,0 +1,38 @@
+<?php
+	$Naam = "Bosschem";
+	$Voornaam = "Indy";
+
+    $volledigeNaam = $Voornaam . " " . $Naam;
+?>
+
+    <!doctype html>
+    <html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht string concatenate</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+
+    <body class="web-backend-opdracht">
+
+        <section class="body">
+
+            <h1>Commentaar:</h1>
+            <ul>
+                <li>
+                    <?php echo $volledigeNaam ?>
+                </li>
+                <li>
+                    <?php echo "Lengte: " . strlen($volledigeNaam) ?>
+                </li>
+            </ul>
+
+        </section>
+
+    </body>
+
+    </html>
