9bf950c77d7c5e12df284c52802a57372c321d05
IndyBoss <32159085+IndyBoss@users.noreply.github.com>
2017-10-17 15:53:31 +0200

    opdracht-sessions

---
+++ opdracht-sessions/opdracht-sessions-1.php
@@ -0,0 +1,59 @@
+<?php
+    session_start();
+
+    	if ( isset( $_GET['session'] ) )
+        {
+            if ( $_GET['session']  == 'destroy' )
+            {
+                session_destroy( );
+                header( 'location: opdracht-sessions-1.php' );
+            }
+        }
+
+
+        $email = ( isset($_SESSION["email"]) ) ? $_SESSION["email"] : '';
+
+        $nickname = ( isset($_SESSION["nickname"]) ) ? $_SESSION["nickname"] : '';
+?>
+    <!doctype html>
+    <html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht sessions</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+
+        <style>
+            .focus {
+                outline: 2px solid #07a550;
+            }
+
+        </style>
+    </head>
+
+    <body class="web-backend-opdracht">
+
+        <h2>Registratiepagina</h2>
+
+        <form method="post" action="opdracht-sessions-2.php">
+            <ul>
+                <li>
+                    <label for="email">E-mail</label>
+                    <input type="text" id="email" name="email" value="<?= $email ?>" <?=( isset( $_GET[ 'focus'] ) && $_GET[ 'focus']=="email" ) ? "class = 'focus'" : '' ?>>
+                </li>
+                <li>
+                    <label for="nickname">Nickname</label>
+                    <input type="text" id="nickname" name="nickname" value="<?= $nickname ?>" <?=( isset( $_GET[ 'focus'] ) && $_GET[ 'focus']=="nickname" ) ? "class = 'focus'" : '' ?>>
+                </li>
+            </ul>
+            <input type="submit" value="Volgende">
+        </form>
+
+        <a href="opdracht-sessions-1.php?session=destroy">Vernietig sessie</a>
+
+
+    </body>
+
+    </html>
